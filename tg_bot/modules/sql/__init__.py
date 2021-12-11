5acbaffde865baf8ab452041207bd7f5f9ba35ac
Aksr aashish <76800327+aksr-aashish@users.noreply.github.com>
2021-12-11 02:38:05 -0800

    Update __init__.py

--- tg_bot/modules/sql/__init__.py
+++ tg_bot/modules/sql/__init__.py
@@ -1,23 +1,15 @@
+from tg_bot import DB_URI
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, scoped_session
-
-from tg_bot import DB_URI, KInit, log
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 
 def start() -> scoped_session:
-    engine = create_engine(DB_URI, client_encoding="utf8", echo=KInit.DEBUG)
-    log.info("[PostgreSQL] Connecting to database......")
+    engine = create_engine(DB_URI, client_encoding="utf8")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
 
 
 BASE = declarative_base()
-try:
-    SESSION = start()
-except Exception as e:
-    log.exception(f'[PostgreSQL] Failed to connect due to {e}')
-    exit()
-   
-log.info("[PostgreSQL] Connection successful, session started.")
+SESSION = start()
